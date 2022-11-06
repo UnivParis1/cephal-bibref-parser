0ef51d52ac84135c5b7c9276206a1ee10e41438e
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2022-11-06 10:21:45 +0100

    Switch to pycurl

--- dump_hal.py
+++ dump_hal.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 from time import sleep
 
-import requests
+import pycurl
+import certifi
+import urllib
+from io import BytesIO
 import argparse
 
-DEFAULT_OUTPUT_FILE_NAME = "halshs_complete.bib"
+DEFAULT_OUTPUT_FILE_NAME = "halshs_complete_1mo.bib"
 DEFAULT_ROWS = 10000
 
-REQUEST_STRING_TEMPLATE = "https://api.archives-ouvertes.fr/search/halshs/?" \
-                          "q=docType_s:(ART OR OUV OR COUV OR COMM OR THESE OR HDR OR REPORT OR NOTICE OR PROCEEDINGS)" \
-                          f"&rows=[ROWS]" \
-                          "&start=[START]" \
-                          "&wt=bibtex"
+URL = "https://api.archives-ouvertes.fr/search/halshs/?"
+
+FILTERS = "docType_s:(ART OR OUV OR COUV OR COMM OR THESE OR HDR OR REPORT OR NOTICE OR PROCEEDINGS)"
 
 MAX_ATTEMPS = 10
 
@@ -42,9 +43,20 @@ if __name__ == '__main__':
     while goon:
         print(start)
         try:
-            request_string = REQUEST_STRING_TEMPLATE.replace('[START]', str(start)).replace('[ROWS]', str(rows))
-            response = requests.get(request_string, timeout=360)
-            str_response = response.content.decode()
+            params = {'rows': rows, 'start': start, 'wt': 'bibtex', 'fq': FILTERS}
+            request_string = URL + urllib.parse.urlencode(params)
+            buffer = BytesIO()
+            c = pycurl.Curl()
+            c.setopt(c.URL, request_string)
+            c.setopt(c.WRITEDATA, buffer)
+            c.setopt(c.CAINFO, certifi.where())
+            c.setopt(c.VERBOSE, True)
+            c.setopt(c.CONNECTTIMEOUT, 36000)
+            c.setopt(c.TIMEOUT, 36000)
+            c.perform()
+            c.close()
+            body = buffer.getvalue()
+            str_response = body.decode()
             if str_response.find("cURL error (28): Timeout was reached") >= 0:
                 raise Exception("Request timeout")
             if str_response.find("cURL error") >= 0:
@@ -59,7 +71,7 @@ if __name__ == '__main__':
                 exit()
             continue
         start += rows
-        goon = len(response.content) > 0
+        goon = len(str_response) > 0
         print(str_response[1:10000])
         attempts = 0
         with open(file, "a") as output_file:

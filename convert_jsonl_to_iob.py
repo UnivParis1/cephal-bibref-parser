73b2a8a32f1592d09d3ac4a0ea62631d68859c60
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2022-11-16 09:48:39 +0100

    better handle IOB output

--- convert_jsonl_to_iob.py
+++ convert_jsonl_to_iob.py
@@ -2,30 +2,31 @@ from convert import convert_to_tags_format
 import json
 from transformers import pipeline
 
+INPUT_FILE = 'allhall.jsonl'
 
-def to_json(annotations, classifier):
-    data = {}
-    counter = 0
-    for annotation in annotations:
-        paragraphs = annotation['paragraphs']
-        if len(paragraphs) == 0:
-            continue
-        paragraph = paragraphs[0]
-        title = paragraph['raw']
-        language = classifier(title[0:512])[0]['label']
-        print(f"{counter} {language}")
-        tokens = paragraph['sentences'][0]['tokens']
-        data.setdefault(language, [])
-        data[language].append({'words': [token['orth'] for token in tokens],
-                               'labels': [token['ner'] for token in tokens]})
-        counter += 1
-    return data
+
+def to_json(counter, annotations, classifier):
+    paragraphs = annotation['paragraphs']
+    if len(paragraphs) == 0:
+        return
+    paragraph = paragraphs[0]
+    title = paragraph['raw']
+    language = classifier(title[0:512])[0]['label']
+    print(f"{counter} {language}")
+    tokens = paragraph['sentences'][0]['tokens']
+    words = [token['orth'] for token in tokens]
+    if ' '.join(words) == paragraph['raw']:
+        value = {'words': words,
+                 'labels': [token['ner'] for token in tokens]}
+        with open(f"bio_tagged/all_tagged_bib_refs_{language}.json", 'a') as file:
+            json.dump(value, file, indent=2)
+    else:
+        print("sentence integrity issue")
+        print(' '.join(words))
+        print(paragraph['raw'])
 
 
 if __name__ == '__main__':
     classifier = pipeline("text-classification", model="papluca/xlm-roberta-base-language-detection")
-    annotations = convert_to_tags_format('auto.jsonl', format='IOB')
-    json_data = to_json(annotations, classifier)
-    for lng, value in json_data.items():
-        with open(f"auto_tagged_bib_refs_{lng}.json", 'a') as file:
-            json.dump(value, file)
+    for index, annotation in enumerate(convert_to_tags_format(INPUT_FILE, format='IOB')):
+        to_json(index, annotation, classifier)

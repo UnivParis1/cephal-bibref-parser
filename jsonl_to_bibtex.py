6adb85add981eda1130957b7975962a17f63fc54
alessandrobuccheri <72658823+alessandrobuccheri@users.noreply.github.com>
2022-11-16 11:35:13 +0100

    Update jsonl_to_bibtex.py

--- jsonl_to_bibtex.py
+++ jsonl_to_bibtex.py
@@ -1,14 +1,17 @@
 import json
+
+
 def jsonl_to_dict(file):
     with open(file, "r", encoding="utf-8") as fl:
         lines = fl.readlines()
         liste_publications = []
         for line in lines:
-            line_dict = json.reads(line)
+            line_dict = json.loads(line)
             opus = {}
             for label in line_dict["label"]:
                 opus.update({label[-1]: line_dict['text'][label[0]:label[1]]})
-            liste_publications += opus
+            id = opus[auteur] + ":" + opus[date]
     return liste_publications
+
 
 a = jsonl_to_dict("All_AB.jsonl")

7ea51e85d83c4565d1e92d7cda29fde0c4472026
alessandrobuccheri <72658823+alessandrobuccheri@users.noreply.github.com>
2022-11-27 08:47:48 +0100

    etiquettes_et_conversion_en_chaine
    J'adapte les étiquettes de doccano à celles du format bibtex.
    
    Ensuite, puisque la librairie bibtexparser est incapable de traduire un dictionnaire en bibtex, j'écris moi-même des chaines de charactères qui ressemblent à du bibtex

--- convert_jsonl_to_bibtex.py
+++ convert_jsonl_to_bibtex.py
@@ -1,4 +1,29 @@
 import json
+import bibtexparser
+
+dictionnaire_etiquettes = {"Auteur": "author",
+ "DatePublication": "year",
+ "Titre": "title",
+ "TitreRevue": "journal",
+ "Volume": "volume",
+ "Numero": "number",
+ "Pages": "pages",
+ "id": "ID",
+ "type": "ENTRYTYPE",
+ "DateFinColloque": "date",
+ "AParaitre": "date",
+ "Collection": "series",
+ "EditeurScientifique": "editor",
+ "TitreOuvrageCollectif": "booktitle",
+ "url": "url",
+ "collaborateur": "series",
+ "EditeurCommerial": "publisher",
+ "LieuPublication": "address",
+ "DOI": "doi",
+ "papges_totales": "pages",
+ "Edition": "edition"
+ }
+
 
 #Crée un dictionnaire à partir du fichier jsonl
 def jsonl_to_dict(file):
@@ -13,11 +38,9 @@ def jsonl_to_dict(file):
                     opus[label[-1]] = opus[label[-1]] + ", " + line_dict['text'][label[0]:label[1]]
                 else:
                     opus.update({label[-1]: line_dict['text'][label[0]:label[1]]})
-            if "DatePublication" in opus.keys():
-                id = opus["Auteur"] + ": " + opus["DatePublication"]
-            else:
-                id = opus["Auteur"] + ": nd"
+            id = line_dict["id"]
             opus.update({"id":id})
+            opus = {dictionnaire_etiquettes.get(k, k): v for k, v in opus.items()}
             liste_publications.append(opus)
     return liste_publications
 
@@ -40,8 +63,21 @@ def type_doc(dictionary):
                 type_de_publi = "article"
         else:
             type_de_publi = "book"
-        entry.update({"type":type_de_publi})
+        entry.update({"ENTRYTYPE":type_de_publi})
     return dictionary
+
+def liste_string(liste_dict):
+    liste_publications_chaine = []
+    for line in liste_dict:
+        chaine = ""
+        chaine += "@" + line["ENTRYTYPE"] + "{" + str(line["ID"])
+        chaine_2 = ""
+        for key, value in line.items():
+            chaine_2 += str(key) + ' = {' + str(value) + "}, "
+        chaine_3 = chaine + chaine_2
+        liste_publications_chaine.append(chaine_3)
+    return(liste_publications_chaine)
 
 liste_dictionnaires = jsonl_to_dict("C:/Users/abuccheri/Desktop/all_AB.jsonl")
 liste_dictionnaires_avec_type = type_doc(liste_dictionnaires)
+liste_documents_chaine = liste_string(liste_dictionnaires_avec_type)
